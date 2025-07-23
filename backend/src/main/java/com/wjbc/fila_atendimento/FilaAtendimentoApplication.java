ecedafd40b374bf4987adba60e5901acf163055f
wellingtonpaim <paim.wellington@gmail.com>
2025-07-23 14:43:55 -0300

    Criar projeto

---
+++ backend/src/main/java/com/wjbc/fila_atendimento/FilaAtendimentoApplication.java
@@ -0,0 +1,13 @@
+package com.wjbc.fila_atendimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilaAtendimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilaAtendimentoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wjbc/fila_atendimento/FilaAtendimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjbc.fila_atendimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilaAtendimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
